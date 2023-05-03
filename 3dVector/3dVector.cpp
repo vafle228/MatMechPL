c2fce06385ba3a1e56243e2cdb67e436a4326435
NeiroFucker <dimasmyslaev228@gmail.com>
2023-05-03 11:38:36 +0500

    Fuck

--- 3dVector/3dVector.cpp
+++ 3dVector/3dVector.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
 #include "Vector.h"
 
+using namespace std;
+
+
+int const a = 1;
 
 int main()
 {
+	double px, py, pz;
+	cin >> px; cin >> py; cin >> pz;
+	Vector position = Vector(px, py, pz);
+	
+	double vx, vy, vz;
+	cin >> vx; cin >> vy; cin >> vz;
+	Vector velocity = Vector(vx, vy, vz);
+
 
 }
